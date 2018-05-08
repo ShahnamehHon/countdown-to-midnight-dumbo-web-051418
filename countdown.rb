f8f3e1c4c86e31276dd1eace955e4130a51a55c2
Ahmadali <ahmadali.azizov@outlook.com>
2018-05-08 17:16:22 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,14 +2,13 @@ int = 12
 def countdown(int)
   while int > 0
     puts "#{int} SECOND(S)!"
-    n -= 1
+    int -= 1
   end
    "HAPPY NEW YEAR!"
 end
  
-def countdown
-def countdown_with_sleep(n)
-  while n > 0
+def countdown_with_sleep(int)
+  while int > 0
     puts "#{n} SECOND(S)!"
     n -= 1
     sleep 1

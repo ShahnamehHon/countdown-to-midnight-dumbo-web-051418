5c6c682c540e4e4ba29d51ef8d96550c980e77c5
Ahmadali <ahmadali.azizov@outlook.com>
2018-05-08 17:16:50 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -9,9 +9,9 @@ end
  
 def countdown_with_sleep(int)
   while int > 0
-    puts "#{n} SECOND(S)!"
-    n -= 1
-    sleep 1
+    puts "#{int} SECOND(S)!"
+    int -= 1
+    sleep(0.8)
   end
    "HAPPY NEW YEAR!"
  end
